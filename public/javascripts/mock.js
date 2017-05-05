2aa73c5a87d2214ebb3b04f8258cf4b5b91b8ddc
fcxie <fcxie@Ctrip.com>
2017-05-05 11:08:12 +0800

    更新mock栏目，加入ace-editor插件，方便在线编写js和json代码

--- public/javascripts/mock.js
+++ public/javascripts/mock.js
@@ -6,16 +6,23 @@ function mockJson(text){
 }
 
 $('#start').on('click',function(){
-    var text = $('#json').val();
-    if(text){
-        var dataStr = mockJson(text);
-        $('#data').text(dataStr);
-    }else{
-        alert('请输入json生成规则')
-    }
+    // var text = $('#json').val();
+    // if(text){
+    //     var dataStr = mockJson(text);
+    //     $('#data').text(dataStr);
+    // }else{
+    //     alert('请输入json生成规则')
+    // }
+    var text = editor.getValue();
+    var dataStr = mockJson(text);
+    $('#data').text(dataStr);
 })
 
 $('#clear').on('click',function(){
     $('#json').val('');
     $('#data').text('');
 })
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.getSession().setMode("ace/mode/javascript");
